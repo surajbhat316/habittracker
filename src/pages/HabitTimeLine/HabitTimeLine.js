7a6921322c176198fa1580dd9d9268dd94ed9f9a
Avineet Bhat <surajbhat316@gmail.com>
2023-10-07 11:56:34 +0530

    added update status functionality

--- src/pages/HabitTimeLine/HabitTimeLine.js
+++ src/pages/HabitTimeLine/HabitTimeLine.js
@@ -1,10 +1,82 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { getRoutine, routineSelector, update } from "../../redux/reducers/routineReducer";
+import "./HabitTimeLine.css";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function HabitTimeLine() {
 
   const {name} = useParams();
-  console.log(name);
+  const dispatch = useDispatch();
+
+  let {routine} = useSelector(routineSelector);
+  console.log(routine);
+  useEffect(()=>{
+    dispatch(getRoutine(name));
+  },[dispatch,name])
+
+
+  async function handleStatus(item, key, val){
+
+    let routineCopy = [...routine];
+    let updatedRoutines = [];
+    routineCopy.forEach((item) => {
+      let itemClone = {...item};
+      if(itemClone.hasOwnProperty(key)){
+        let newList = [];
+        itemClone[key].forEach((v) => {
+            let clonedObject = {...v};
+            if(clonedObject.date === val.date && clonedObject.day === val.day && clonedObject.year === val.year){
+              clonedObject.completed = !clonedObject.completed; 
+            }
+            newList.push(clonedObject);
+        })
+        itemClone[key] = [...newList]
+        updatedRoutines.push(itemClone);
+      }else{
+        updatedRoutines.push(itemClone);
+      }
+    })
+    dispatch(update(updatedRoutines));
+
+    async function updateDB(){
+      const habitDocRef = doc(db, "habits", name);
+
+      await updateDoc(habitDocRef, {
+        routine: [...updatedRoutines]
+      });
+    }
+
+    updateDB();
+
+  }
   return (
-    <div>HabitTimeLine</div>
+    <div>
+      <div>
+      {routine.map((item,i) => {
+        let key = "week_"+i;
+        return (
+          <div key={i}>
+            <h2>Week {i} of {name}</h2>
+            <div className="container1">
+            {item[key].map((val, j) => {
+              return (
+                    <div key={j}>
+                      <p>{val.day}</p>
+                      <p>{val.date} / {val.month} / {val.year}</p>
+                      <p>{val.completed? "Completed" :"In Progress"}</p>
+                      <button onClick={() => handleStatus(item, key, val)}>{!val.completed?"Mark as Completed": "Mark as In Progress"}</button>
+                    </div>
+              );
+            })}
+            </div>
+          </div>
+        );
+      })}
+      </div>
+
+    </div>
   )
 }
--- src/redux/reducers/routineReducer.js
+++ src/redux/reducers/routineReducer.js
@@ -1,17 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
-const INITIAL_STATE = { routine: ["Abc"] };
+const INITIAL_STATE = { routine: [] };
+
+export const getRoutine = createAsyncThunk(
+    "getRoutine",
+    (arg, thunkApi) => {
+        async function getData(){
+            const docRef = doc(db, "habits", arg);
+            const docSnap = await getDoc(docRef);
+      
+            if (docSnap.exists()) {
+              console.log("Document data:", docSnap.data());
+              thunkApi.dispatch(add(docSnap.data()));
+            } else {
+              // docSnap.data() will be undefined in this case
+              console.log("No such document!");
+              thunkApi.dispatch(add({"routine": []}));
+            }
+          }
+          getData();
+    }
+);
 
 
 
 const routineSlice = createSlice({
     name: "routine",
     initialState: INITIAL_STATE,
-    reducers: []
+    reducers: {
+        add: (state, action)=>{
+            state.routine = [];
+            state.routine = [...action.payload.routine]
+        },
+        update: (state, action) =>{
+            state.routine = [];
+            state.routine = [...action.payload];
+        }
+    }
 })
 
 
 export const routineReducer = routineSlice.reducer;
+
+export const { add, update } = routineSlice.actions;
 
 
 export const routineSelector = (state) => state.routineReducer;
